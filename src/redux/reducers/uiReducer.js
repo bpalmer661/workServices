738015b2793709349115c872d6d1abd7c559b87d
bpalmer661 <59478417+bpalmer661@users.noreply.github.com>
2020-10-16 16:30:47 +0800

    lesson-22
    lesson-22

---
+++ src/redux/reducers/uiReducer.js
@@ -0,0 +1,38 @@
+import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI, SET_AUTHENTICATED} from '../types'
+
+const initialState = {
+    loading: false,
+    errors: null
+};
+
+export default function(state = initialState, action){
+
+
+switch(action.type){
+
+    case SET_ERRORS:
+        return {
+            ...state,
+            loading: false,
+            errors: action.payload
+        };
+        
+        case CLEAR_ERRORS:
+            return{
+                ...state,
+                loading: false,
+                errors: null
+            }
+
+case LOADING_UI: 
+return {
+    ...state,
+    loading: true
+};
+
+
+            default:
+                return state;
+}
+
+}
